51821eca32c6da10dab47d1f00fc2ee95c4af798
Naim <kntr.naim@gmail.com>
2026-01-27 10:43:50 +0000

    created a history html to track added workouts

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert('test');
+const year = 2025
+const date = prompt("de quelle année etes vous");
+const age = year-date;
+
+if (age>=18){
+    console.log("tie un bon");
+}else{
+    console.log("noob");
+}
