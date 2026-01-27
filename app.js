9d844e4dd4e0e6839072fc860947b6a8bda11b41
Naim <kntr.naim@gmail.com>
2026-01-27 11:56:52 +0000

    added form listener that creates array to stock created workouts

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const year = 2025
-const date = prompt("de quelle année etes vous");
-const age = year-date;
+const form = document.getElementById("seance-form");
+const submitBtn = document.getElementById("submit-button");
+let seances = []
 
-if (age>=18){
-    console.log("tie un bon");
-}else{
-    console.log("noob");
-}
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+    const seance={
+        exercice:document.getElementById("workout-name").value,
+        poids:Number(document.getElementById("weight").value),
+        series:Number(document.getElementById("series-count").value),
+        repetitions:Number(document.getElementById("repetitions-count").value),
+        date:new Date().toLocaleDateString()
+    };
+    seances.push(seance);
+    console.log(seances);
+
+    form.reset();
+
+    const texteInitial = submitBtn.textContent;
+    submitBtn.textContent ="Séance créée !";
+    submitBtn.disabled=true;
+
+    setTimeout(()=>{
+        submitBtn.textContent = texteInitial;
+        submitBtn.disabled = false;
+
+    },1000);
+});
